41fcf202f06d404754edfb450cbf77cfba9aa4aa
Amirol Ahmad <AmirolAhmad@users.noreply.github.com>
2023-08-16 21:20:27 +0800

    separate views

--- antiscamV2/antiscam/urls.py
+++ antiscamV2/antiscam/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path
 
 from . import views
+from .views_auth import register, signin, signout, activate
+from .views_dashboard import dashboard
 
 # app_name = "antiscam"
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('register/', views.register, name='register'),
-    path('signin/', views.signin, name='signin'),
-    path('signout/', views.signout, name='signout'),
-    path('activate/<uidb64>/<token>', views.activate, name='activate'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    #authentication
+    path('register/', register, name='register'),
+    path('signin/', signin, name='signin'),
+    path('signout/', signout, name='signout'),
+    path('activate/<uidb64>/<token>', activate, name='activate'),
+    #dashboard
+    path('dashboard/', dashboard, name='dashboard'),
 ]
