95df3ebb58c54e9b3708b73d28b66bb6b2d0c9d3
Amirol Ahmad <AmirolAhmad@users.noreply.github.com>
2023-08-17 13:07:09 +0800

    added location and category

--- antiscamV2/antiscam/views_dashboard.py
+++ antiscamV2/antiscam/views_dashboard.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from .models import Scammer
+from .models import Scammer, Location, Category
 
 @login_required(login_url='/signin/')
 def dashboard(request):
@@ -13,12 +13,20 @@ def profile(request):
 
 @login_required(login_url='/signin/')
 def newscammer(request):
+    locations = Location.objects.all()  # Fetch locations from the database
+    categories = Category.objects.all()  # Fetch locations from the database
+    context = {'locations': locations, 'categories': categories}
+    
     if request.method == "POST":
         name = request.POST['name']
         brief = request.POST['brief']
         modus = request.POST['modus']
         date_reported = request.POST['date_reported']
         last_date_reported = timezone.now().date()
+        location_id = request.POST['location']
+        location = Location.objects.get(id=location_id)
+        category_id = request.POST['category']
+        category = Category.objects.get(id=category_id)
 
         # Get the currently logged-in user
         reported_by = request.user
@@ -29,11 +37,13 @@ def newscammer(request):
             brief_intro=brief,
             modus_operandi=modus,
             date_reported=date_reported,
-            last_date_reported=last_date_reported
+            last_date_reported=last_date_reported,
+            location = location,
+            category = category
         )
         scammer.save()
         
         # Redirect to a success page or another view
         return redirect('dashboard')
 
-    return render(request,"scammers/new.html")
+    return render(request,"scammers/new.html", context)
