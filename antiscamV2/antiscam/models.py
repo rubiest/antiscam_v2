484e02c0cd7c98c178885aac624addf5b4e3e81f
Amirol Ahmad <AmirolAhmad@users.noreply.github.com>
2023-08-17 12:34:40 +0800

    added more field to db

--- antiscamV2/antiscam/models.py
+++ antiscamV2/antiscam/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from datetime import datetime
 import random
 
+class Location(models.Model):
+    name = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.name
+    
 class CustomUser(AbstractUser):
     is_activation_used = models.BooleanField(default=False)
     token = models.CharField(max_length=255, blank=True, null=True, default='')
     phone = models.CharField(max_length=13, default='')
-    location = models.CharField(max_length=30, default='')
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
     profile_number = models.CharField(max_length=7, default='')
     old_email = models.CharField(max_length=255, blank=True, null=True, default='')
 
@@ -21,12 +32,14 @@ class CustomUser(AbstractUser):
 
 class Scammer(models.Model):
     reported_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, default='')
     brief_intro = models.CharField(max_length=255)
     modus_operandi = models.TextField(default='')
     is_verified = models.BooleanField(default=False)
-    date_reported = models.DateField(default='')
-    last_date_reported = models.DateField(default='', null=True, blank=True)
+    date_reported = models.DateField()
+    last_date_reported = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
---
+++ antiscamV2/seed.py
@@ -0,0 +1,45 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'antiscamV2.settings')
+django.setup()
+
+from antiscam.models import Location, Category
+
+location_names = [
+    "Johor",
+    "Kedah",
+    "Kelantan",
+    "Melaka",
+    "Negeri Sembilan",
+    "Pahang",
+    "Perak",
+    "Perlis",
+    "Pulau Pinang",
+    "Sabah",
+    "Sarawak",
+    "Selangor",
+    "Terengganu",
+    "Wilayah Persekutuan Kuala Lumpur",
+    "Wilayah Persekutuan Labuan",
+    "Wilayah Persekutuan Putrajaya",
+]
+
+category_names = [
+    "Investment Scams",
+    "Romance Scams",
+    "Lottery or Prize Scams",
+    "Job Scams",
+    "Charity Scams",
+    "Impersonation Scams",
+    "Vishing (Voice Phishing)",
+    "Smishing (SMS Phishing)",
+]
+
+for location_name in location_names:
+    Location.objects.get_or_create(name=location_name)
+
+for category_name in category_names:
+    Category.objects.get_or_create(name=category_name)
+
+print("Seeding completed.")
